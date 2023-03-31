1f623e3fae1d187ce81abd08a4bf600a7a77af49
unknown <aktechinfos@gmail.com>
2023-03-31 10:29:56 +0530

    made some changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Done! This p  roject has been deployed successfully</h1>
+      <h1>Done! This project has been deployed successfully</h1>
     </div>
   );
 }
